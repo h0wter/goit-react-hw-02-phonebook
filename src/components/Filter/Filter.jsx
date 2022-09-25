d2a33d6dcdca1b9af8bfcc8a64e4237b298efe7e
h0wter <howter99@gmail.com>
2022-09-25 19:46:37 +0300

    add filter component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,12 @@
+import { Box } from 'components/Box';
+
+export const Filter = ({ onChange }) => {
+  return (
+    <>
+      <label htmlFor="filter">Find contacts by name</label>
+      <Box mt="5px" mb="15px">
+        <input name="filter" type="text" onChange={onChange} />
+      </Box>
+    </>
+  );
+};

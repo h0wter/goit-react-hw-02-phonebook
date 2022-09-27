a666361692526b703112f9f9f3fe0cdc8240e7d7
h0wter <howter99@gmail.com>
2022-09-27 20:29:56 +0300

    rename contacts list component

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { Contact } from 'components/Contact/Contact';
+
+export const ContactList = ({ contacts, onClick }) => {
+  return (
+    <>
+      <ul>
+        {contacts.map(contact => {
+          return (
+            <Contact
+              contact={contact}
+              key={contact.id}
+              onClick={onClick}
+            ></Contact>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onClick: PropTypes.func.isRequired,
+};
